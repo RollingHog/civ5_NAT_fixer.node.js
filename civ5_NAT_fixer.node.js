a85abb87c5790263fb0ceb222b60725cc2a0318a
RollingHog <mpaushev2@mail.ru>
2022-04-06 02:50:14 +0300

    tracert+ipconfig functions

--- civ5_NAT_fixer.node.js
+++ civ5_NAT_fixer.node.js
@@ -1,6 +1,7 @@
-const { networkInterfaces } = require('os')
+const os = require('os')
+const child_process = require('child_process')
 
-// misc
+// MISC
 const log = console.log
 
 function pause() {
@@ -9,20 +10,41 @@ function pause() {
   process.stdin.on( 'data', () => process.stdin.pause())
 }
 
-const nets = networkInterfaces()
-const results = {}
+// MAIN THINGS
 
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        if (net.family === 'IPv4' && !net.internal) {
-            if (!results[name]) {
-                results[name] = []
-            }
-            results[name].push(net)
-        }
-    }
+function tracert(dest) {
+  log('doing tracert; this is long process')
+  // try {
+  // } catch(e) {
+  //   autoenc.detectEncoding(e.stderr).text
+  // }
+  const result = child_process.execSync('tracert ' + dest).toString()
+    .split('\n')
+    .filter(e => e.match(/^( +\d+)/g))
+    .map(e => e.split(' ').slice(-2))
+    .map(e => ([e[0], e[1].replace(/[[\]]/g,'')]) )
+  log('tracert done')
+  return result
 }
 
-log(results['Hamachi'], results['Ethernet'])
-pause()
+function ipconfig_() {
+  const nets = os.networkInterfaces()
+  const result = {}
 
+  for (const name of Object.keys(nets)) {
+      for (const net of nets[name]) {
+          if (net.family === 'IPv4' && !net.internal) {
+              if (!result[name]) {
+                  result[name] = []
+              }
+              result[name].push(net)
+          }
+      }
+  }
+  return result
+}
+
+const interfaces = ipconfig_()
+log(interfaces['Hamachi'], interfaces['Ethernet'])
+log(tracert('ya.ru'))
+pause()
