3e9d89346de2d1d5e48b7ea5e221c359955f4150
RollingHog <mpaushev2@mail.ru>
2022-04-07 03:36:01 +0300

    added getVPNData() and createBatFilesFromTargetVars(), moved data aquiring to getTargetVars()

--- civ5_NAT_fixer.node.js
+++ civ5_NAT_fixer.node.js
@@ -1,4 +1,5 @@
 //BUILT-IN LIBS
+const fs = require('fs')
 const os = require('os')
 const child_process = require('child_process')
 const http = require('http')
@@ -7,6 +8,7 @@ const http = require('http')
 
 //CONSTANTS
 const IP_DISCOVERY_SERVICE_ADDR = 'icanhazip.com'
+let   TRACERT_PROBE_ADDR = 'ya.ru'
 
 const HAMACHI_ADAPTER = 'Hamachi'
 const ETHERNET_ADAPTER = 'Ethernet'
@@ -26,6 +28,13 @@ function pause(prompt = 'Press any key...') {
   process.stdin.setRawMode( true )
   process.stdin.resume()
   process.stdin.on( 'data', () => process.stdin.pause())
+}
+
+function getChar() {
+  //from https://stackoverflow.com/questions/20185548/how-do-i-read-a-single-character-from-stdin-synchronously
+  let buffer = Buffer.alloc(1)
+  fs.readSync(0, buffer, 0, 1)
+  return buffer.toString('utf8')
 }
 
 // MAIN THINGS
@@ -50,6 +59,7 @@ function tracert(dest) {
 }
 
 function ipconfig() {
+  log('doing ipconfig')
   const nets = os.networkInterfaces()
   const result = {}
 
@@ -65,6 +75,7 @@ function ipconfig() {
   }
 
   cache[ipconfig.name] = result
+  log('ipconfig done')
   return result
 }
 
@@ -97,35 +108,79 @@ function findNAT(tracert_array) {
   return result
 }
 
-async function getTargetConstants() {
+const POSSIBLE_HAMACHI_LOCATIONS = [
+  'C:\\Program Files\\LogMeIn Hamachi\\x64',
+  'C:\\Program Files (x86)\\LogMeIn Hamachi\\x64',
+  'D:\\Program Files\\LogMeIn Hamachi\\x64',
+  'D:\\Program Files (x86)\\LogMeIn Hamachi\\x64',
+]
+
+function getVPNData() {
+  let res
+
+  HAMACHI_EXEC_LOCATION = POSSIBLE_HAMACHI_LOCATIONS[1]
+  try {
+    res = child_process.execSync(`"${HAMACHI_EXEC_LOCATION}\\hamachi-2.exe" --cli`).toString()
+  } catch(e) {
+    if(e.status == 1) {
+      // Hamachi returns 1 as exitcode with this command, don't ask me why
+      res = e.stdout.toString()
+    } else {
+      throw new Error(JSON.stringify(e))
+    }
+  }
+
+  res = res.replace(/\r/g, '')
+
+  const address = res.match('address *: *([^\n]+)')[1]
+    .replace(/ +/g, ' ')
+    .split(' ')[0]
+
+  const nickname = res.match('nickname *: *([^\n]+)')[1]
+
+  return { address, nickname }
+}
+
+async function getTargetVars() {
   // this set of parameters can be used at any number of CALLER computers once obtained
+  ipconfig()
+  tracert(TRACERT_PROBE_ADDR)
   return {
     TARGET_LAN_IP:  cache.ipconfig[ETHERNET_ADAPTER][0].address,
     TARGET_MASK:    cache.ipconfig[ETHERNET_ADAPTER][0].netmask,
     // ip of first hop in tracert
     TARGET_ROUTER:  cache.tracert[0].ip,
     target_NAT_IP:  findNAT(cache.tracert).ip,
-    target_VPN_IP:  null,
+    target_VPN_IP:  getVPNData().nickname,
   }
+}
+
+async function createBatFilesFromTargetVars(vars) {
+  //form .bat files "set" and "clear" once parameters aquired
+  const ROUTE_ADDER_FILENAME = 'civ5_routes_add.bat'
+  const adderText = `@echo off
+rem set /p=Run on CALLER computer only. Enter to proceed.
+echo route add ${vars.TARGET_LAN_IP} mask 255.255.255.0       ${vars.TARGET_VPN_IP}
+echo route add ${vars.TARGET_ROUTER} mask 255.255.255.0       ${vars.TARGET_LAN_IP}
+echo route add ${vars.TARGET_NAT_IP} mask ${vars.TARGET_MASK} ${vars.TARGET_ROUTER}
+rem delete ${ROUTE_ADDER_FILENAME}
+  `
+  await fs.promises.writeFile(ROUTE_ADDER_FILENAME, adderText, { encoding:'ascii' })
+  log(`writing ${ROUTE_ADDER_FILENAME} done`)
 }
 
 async function main() {
   //ping (it does ICMP) to CALLER computer to check if redoing is nessesary
   // https://stackoverflow.com/questions/4737130/how-to-ping-from-a-node-js-app
 
-  // https://stackoverflow.com/questions/20185548/how-do-i-read-a-single-character-from-stdin-synchronously
-
-  await getPublicIP()
-  const interfaces = ipconfig()
-  // log(interfaces[HAMACHI_ADAPTER], interfaces[ETHERNET_ADAPTER])
-  // at-home
-  tracert('at-home.ru')
-  log(await getTargetConstants())
-  //form .bat files "set" and "clear" once parameters aquired
+  // await getPublicIP()
+  const vars = await getTargetVars()
+  createBatFilesFromTargetVars(vars)
   pause()
 }
 
 main()
 .catch( e => {
-  console.log('FATAL ERROR: ' + e.message)
+  console.log('FATAL: ' + e.message)
+  pause()
 })
